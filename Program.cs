c61c95a89014546b5fb669c0a1f3fafe912b567d
kristycurrier <kristy.currier@gmail.com>
2018-08-24 13:42:11 -0400

    DateTime input taken from the use done

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace Proj01Part02
     {
         static void Main(string[] args)
         {
+            /*
+            string line = Console.ReadLine();
+            DateTime dt;
+            while (!DateTime.TryParseExact(line, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dt))
+            {
+                Console.WriteLine("Invalid date, please retry");
+                line = Console.ReadLine();
+            }
+            */
+            DateTime date1;
+            DateTime date2;
+
+            Console.WriteLine("Enter two dates for the time between them in days, hours and minutes.");
+            Console.WriteLine("Please enter first date: ");
+            date1 = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second date: ");
+            date2 = DateTime.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine(date1.ToString());
+            Console.ReadKey();
+            
         }
     }
 }
